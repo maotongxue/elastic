3cdb6dc0d0efa066520cd0cf2482b2103db689df
poison <maoguangzheng@outlook.com>
2018-05-28 17:59:09 +0800

    update to 6.2.4

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 chmod 777 -R ./elasticsearch/esdata
 chmod 777 -R ./logstash/logs
 chmod 777 -R ./logstash/pipeline
+echo 'vm.max_map_count=262144' >> /etc/sysctl.conf
+sysctl -w vm.max_map_count=262144
